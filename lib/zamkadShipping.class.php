142461aa62e37a9867245933d90ec6ba081ef3ec
Serge Rodovnichenko <sergerod@gmail.com>
2021-05-11 01:53:40 +0300

    Улучшил проверку

--- lib/zamkadShipping.class.php
+++ lib/zamkadShipping.class.php
@@ -51,7 +51,7 @@ class zamkadShipping extends waShipping
             'description'  => 'Плата за каждый полный и неполный км.',
             'field_type'   => 'number',
             'min'          => 1,
-            'max'          => $last['to'] ?: 1,
+            'max'          => ($last['to'] > 1 ? $last['to'] : 1),
             'step'         => 1,
             'required'     => 1,
             'data'         => ['affects-rate' => true]
